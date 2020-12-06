554a1f16c4af3fef1966873fd077996da3457ffd
Kifixo <danielhuici@hotmail.com>
2020-12-06 16:30:26 +0100

    Hostfix (broken frontend)

--- src/main/java/urlshortener/config/WebSecurityConfiguration.java
+++ src/main/java/urlshortener/config/WebSecurityConfiguration.java
@@ -26,6 +26,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+      /*
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
@@ -40,6 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login").and().exceptionHandling()
                 .accessDeniedPage("/login");
+       */
     }
 
     @Override
