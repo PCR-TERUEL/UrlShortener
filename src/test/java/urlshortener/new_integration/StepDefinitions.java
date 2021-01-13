6fde5714ec2656eb4b176e869bc8c1f098aeb47c
Kifixo <danielhuici@hotmail.com>
2021-01-13 02:33:03 +0100

    More travis...

--- src/test/java/urlshortener/new_integration/StepDefinitions.java
+++ src/test/java/urlshortener/new_integration/StepDefinitions.java
@@ -25,10 +25,8 @@ public class StepDefinitions {
 
     @Given("Open the Chrome and launch the application")
     public void openChromeAndLaunch() throws Throwable {
-        WebDriverManager.edgedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost:8080/");
     }
