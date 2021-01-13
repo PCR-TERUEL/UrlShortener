e64fa5dcaf699eb4e41b3c6a706264c6de74e246
Kifixo <danielhuici@hotmail.com>
2021-01-13 03:59:17 +0100

    Still debgging...

--- src/test/java/urlshortener/new_integration/StepDefinitions.java
+++ src/test/java/urlshortener/new_integration/StepDefinitions.java
@@ -105,7 +105,7 @@ public class StepDefinitions {
         List<WebElement> tableRows = baseTable.findElements(By.tagName("tr"));
         assertFalse(tableRows.isEmpty());
         String row = tableRows.get(1).getAttribute("innerHTML");
-        System.out.println(row);
+        assertEquals("a",row);
         String[] fields = row.split("</td>");
         assertEquals(true, fields[1].contains("href"));
     }
